904dfcad7ed77867a57a806be602501973eb93f5
verstappenpush <thiagojurema445@gmail.com>
2026-07-07 07:55:20 -0300

    Revert "fix: sucompat, __init/__exit macros (legacy) (#1376)"
    This reverts commit 13ad2b4cbdad7b14324a4f7867110bb86a8dc411.

--- kernel/feature/sulog.h
+++ kernel/feature/sulog.h
@@ -2,10 +2,9 @@
 #define __KSU_H_SULOG
 
 #include <linux/types.h>
-#include <linux/init.h>
 
 bool ksu_sulog_is_enabled(void);
 void __init ksu_sulog_init(void);
-void ksu_sulog_exit(void);
+void __exit ksu_sulog_exit(void);
 
 #endif
